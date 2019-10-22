3295f19220ebda6608c53371791174b07ed4e27b
chrisitian.sloper@gmail.com <Christian Sloper>
2019-10-22 22:26:02 +0200

    stresser med å få til tokens.

--- chessredditbot/app.py
+++ chessredditbot/app.py
@@ -1,23 +1,79 @@
 import sqlite3 as sql
 
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import database
+import praw
 from datetime import datetime,timezone
+import config
 app = Flask(__name__)
 
 DATABASE = 'database.db'
 
+the_token = ""
+
 @app.route("/")
-def hello():
+def homepage():
+    text = 'blah'
 
-    time_since_creation, image_link = database.get_latest()
 
-    time_since_creation = str(datetime.now(timezone.utc) - datetime.fromtimestamp(time_since_creation,tz=timezone.utc))
+    if not the_token:
+        
+        reddit = praw.Reddit(
+            client_id=config.client_id,
+            client_secret=config.client_secret,
+            user_agent=config.user_agent,
+            redirect_uri='http://127.0.0.1:5000/authorize_callback'
+            )
+
+
+        text = f" I dont have the token: {reddit.auth.url(['identity'], '...','permanent')}"
+
+        
+        
+    else:
+        text =" I do have the token "
+
+    # link_no_refresh = reddit.get_authorize_url('UniqueKey')
+    # link_refresh = reddit.get_authorize_url('DifferentUniqueKey',
+    #                                    refreshable=True)
+    # link_no_refresh = "<a href=%s>link</a>" % link_no_refresh
+    # link_refresh = "<a href=%s>link</a>" % link_refresh
+    # text = "First link. Not refreshable %s</br></br>" % link_no_refresh
+    # text += "Second link. Refreshable %s</br></br>" % link_refresh
+    return text
+
+    #time_since_creation, image_link = database.get_latest()
+
+    #time_since_creation = str(datetime.now(timezone.utc) - datetime.fromtimestamp(time_since_creation,tz=timezone.utc))
     
 
-    return render_template("front_page.html", time=time_since_creation, image=image_link)
+    #return render_template("front_page.html", time=time_since_creation, image=image_link)
     
+
+@app.route('/authorize_callback')
+def authorized():
+    state = request.args.get('state', '')
+    code = request.args.get('code', '')
+    #info = reddit.get_access_information(code)
+    #user = reddit.get_me()
+    info = "unknown info"
+    user = "unknown user"
+    variables_text = "State=%s, code=%s, info=%s." % (state, code,
+                                                      info)
+    
+    the_token = code
+
+    return variables_text + "<br><br>the correct code idiot: " + reddit.auth.authorize(code) + "<br><br>" + str(reddit.user.me())
 
 if __name__ == "__main__":
+    reddit = praw.Reddit(
+            client_id=config.client_id,
+            client_secret=config.client_secret,
+            user_agent=config.user_agent,
+            refresh_token=config.refresh_token
+            )
+
+    print(reddit.auth.scopes())
     
-    app.run(host='0.0.0.0')
+
+    app.run(debug=True, port=5000)
--- chessredditbot/readbot.py
+++ chessredditbot/readbot.py
@@ -7,12 +7,17 @@ import database as database
 
 reddit = praw.Reddit(client_id=config.client_id,
             client_secret=config.client_secret,
-            user_agent=config.user_agent)
+            user_agent=config.user_agent,
+            username=config.username,
+            password=config.password)
 
+
+
+_subreddit = 'pythonforengineers'
 
 c = None
 cnt = 0
-for comment in reddit.subreddit('chess').comments(limit=1000):
+for comment in reddit.subreddit(_subreddit).comments(limit=1000):
     cnt +=1
 
     if re.search("(?i)reset the counter",comment.body):
@@ -20,7 +25,8 @@ for comment in reddit.subreddit('chess').comments(limit=1000):
         print(comment, comment.body, comment.created_utc)
         
         if database.submission_is_new(comment.submission.id):
-            database.record_submisson(comment.submission.id, comment.id, comment.submission.created_utc, 0, comment.created_utc, comment.submission.url)
+            #database.record_submisson(comment.submission.id, comment.id, comment.submission.created_utc, 0, comment.created_utc, comment.submission.url)
+            comment.reply("and here is the reply")
 
         
 print(cnt)
